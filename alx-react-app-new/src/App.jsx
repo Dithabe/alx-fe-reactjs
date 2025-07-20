88d1788e96314fb02e254b83436be7bb840b5e2e
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-07-20 23:59:06 +0200

    Create App.jsx

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
+
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;

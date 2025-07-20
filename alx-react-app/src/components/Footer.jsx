8d241945c4e6430b5959af4401c133a613005842
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-07-20 23:44:00 +0200

    Create Footer.jsx

---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#f1f1f1', color: '#333', textAlign: 'center', padding: '10px' }}>
+      <p>&copy; 2025 My Travel Blog</p>
+    </footer>
+  );
+}
+
+export default Footer;

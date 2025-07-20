911a230ce3514ac2471379e042191fa76ea90315
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-07-20 23:45:29 +0200

    Create MainContent.jsx

---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', fontFamily: 'Arial', lineHeight: '1.6' }}>
+      <p>Welcome to my travel blog. Here are some of my favorite cities!</p>
+    </main>
+  );
+}
+
+export default MainContent;
